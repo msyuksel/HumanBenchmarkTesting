c1dd17ab2293eea1ff9728af637e0b328987319e
msyuksel <55067213+msyuksel@users.noreply.github.com>
2023-02-23 15:26:42 -0600

    Formatting

--- src/main/java/pageobjects/LandingPage.java
+++ src/main/java/pageobjects/LandingPage.java
@@ -5,65 +5,66 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-
 public class LandingPage {
-	//This class has page objects/elements/methods and links related to the main page of the website
-	//This will driver the driver.get method of getting to the main page.
+	// This class has page objects/elements/methods and links related to the main
+	// page of the website
+	// This will driver the driver.get method of getting to the main page.
 	WebDriver driver;
 
 	public LandingPage(WebDriver driver) {
-		//super(driver);
+		// super(driver);
 		// initialization
 		this.driver = driver;
 		PageFactory.initElements(driver, this);
 
 	}
+
 	@FindBy(css = "div[class = \"css-1qvtbrk e19owgy78\"]:nth-child(2) h1 ")
 	WebElement landingPageConifrmation1;
-	
+
 	@FindBy(css = "div[class = \"css-1qvtbrk e19owgy78\"]:nth-child(2) h2 ")
 	WebElement landingPageConifrmation2;
-	
+
 	@FindBy(xpath = "//a[contains(text(),'Get Started')]")
 	WebElement getStarted;
-	
+
 	@FindBy(css = "div[class = \"css-1ur49oz e19owgy70\"]:nth-child(1) a[class = \"css-uaat4j e19owgy72\"]:nth-child(1)")
 	WebElement reactionTimeLinkBtn;
-	
+
 	@FindBy(css = "div[class = \"css-1ur49oz e19owgy70\"]:nth-child(1) a[class = \"css-uaat4j e19owgy72\"]:nth-child(2)")
 	WebElement sequenceMemoryLinkBtn;
-	
+
 	@FindBy(css = "div[class = \"css-1ur49oz e19owgy70\"]:nth-child(1) a[class = \"css-uaat4j e19owgy72\"]:nth-child(3)")
 	WebElement aimTrainerLinkBtn;
-	
+
 	@FindBy(css = "div[class = \"css-1ur49oz e19owgy70\"]:nth-child(2) a[class = \"css-uaat4j e19owgy72\"]:nth-child(1)")
 	WebElement numberMemoryLinkBtn;
-	
+
 	@FindBy(css = "div[class = \"css-1ur49oz e19owgy70\"]:nth-child(2) a[class = \"css-uaat4j e19owgy72\"]:nth-child(2)")
 	WebElement verbalMemoryLinkBtn;
-	
+
 	@FindBy(css = "div[class = \"css-1ur49oz e19owgy70\"]:nth-child(2) a[class = \"css-uaat4j e19owgy72\"]:nth-child(3)")
 	WebElement chimpTestLinkBtn;
-	
+
 	@FindBy(css = "div[class = \"css-1ur49oz e19owgy70\"]:nth-child(3) a[class = \"css-uaat4j e19owgy72\"]:nth-child(1)")
 	WebElement visualMemoryLinkBtn;
-	
+
 	@FindBy(css = "div[class = \"css-1ur49oz e19owgy70\"]:nth-child(3) a[class = \"css-uaat4j e19owgy72\"]:nth-child(2)")
 	WebElement typingLinkBtn;
-	
-    @FindBy(css = "div[class = \"css-w0151b e19owgy76\"] p:nth-child(1)")
+
+	@FindBy(css = "div[class = \"css-w0151b e19owgy76\"] p:nth-child(1)")
 	WebElement copyRightLocator;
-    
-      @FindBy(css = "div[class = \"css-w0151b e19owgy76\"] p:nth-child(2)")
+
+	@FindBy(css = "div[class = \"css-w0151b e19owgy76\"] p:nth-child(2)")
 	WebElement contactEmailLocator;
 
-    @FindBy(css = "div[class = \"css-w0151b e19owgy76\"] p:nth-child(3)")
+	@FindBy(css = "div[class = \"css-w0151b e19owgy76\"] p:nth-child(3)")
 	WebElement privacyPolicyLocator;
 
-    @FindBy(css = "div[class = \"css-w0151b e19owgy76\"] p:nth-child(4)")
+	@FindBy(css = "div[class = \"css-w0151b e19owgy76\"] p:nth-child(4)")
 	WebElement licensingLocator;
-    
-	//This method will take us directly to the landing page
+
+	// This method will take us directly to the landing page
 	public void goTo() {
 		driver.get("https://humanbenchmark.com/");
 	}

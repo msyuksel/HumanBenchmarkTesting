0600ec6af0d972b170228a6e88ba9e9cce021279
msyuksel <55067213+msyuksel@users.noreply.github.com>
2023-02-27 13:59:07 -0600

    Added new page object file and updated ReactionTimePage
    Forgot to add the test start page object. oops

--- src/main/java/pageobjects/SequenceMemoryPage.java
+++ src/main/java/pageobjects/SequenceMemoryPage.java
@@ -5,19 +5,15 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-public class ReactionTimePage {
+public class SequenceMemoryPage {
     WebDriver driver;
 
-    public ReactionTimePage(WebDriver driver) {
+    public SequenceMemoryPage(WebDriver driver) {
         // super(driver);
         // initialization
         this.driver = driver;
         PageFactory.initElements(driver, this);
     }
-
-    @FindBy(css = "div[class=\"view-splash e18o0sx0 css-saet2v e19owgy77\"]")
-    WebElement reactionTimeStartTestBtn;
-
     @FindBy(css = "div[class = \"view-go e18o0sx0 css-saet2v e19owgy77\"] div div div h1 div")
     WebElement reactionTimeClickBtn;
 
@@ -26,7 +22,6 @@ public class ReactionTimePage {
     WebElement saveScoreBtn;
 
     public void goTo() {
-        driver.get("https://humanbenchmark.com/tests/reactiontime");
+        driver.get("https://humanbenchmark.com/tests/sequence");
     }
-
 }

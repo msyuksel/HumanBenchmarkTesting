5d788de19f17729cf895028191ba63160fd0f44d
msyuksel <55067213+msyuksel@users.noreply.github.com>
2023-02-27 19:19:34 -0600

    Mostly finished SequenceMemoryPage, but also changed AbstractComponent a little. Should not hurt any other code

--- src/main/java/abstractComponents/AbstractComponent.java
+++ src/main/java/abstractComponents/AbstractComponent.java
@@ -1,6 +1,5 @@
 package abstractComponents;
 
-import java.time.Duration;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -9,6 +8,8 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class AbstractComponent {
 	//This class points to page objects/elements/methods all pages in the website will have access to.
@@ -52,11 +53,11 @@ public class AbstractComponent {
 
 	
 
-	public void waitForElementToDisappear(WebElement ele) throws InterruptedException {
-		Thread.sleep(1000);
-	    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(2));
-		wait.until(ExpectedConditions.invisibilityOf(ele));
+	public void waitForElementToDisappear(WebElement findBy) throws InterruptedException {
+	    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+		wait.until(ExpectedConditions.invisibilityOf(findBy));
 
 	}
+
 
 }
--- src/main/java/pageobjects/SequenceMemoryPage.java
+++ src/main/java/pageobjects/SequenceMemoryPage.java
@@ -1,27 +1,100 @@
 package pageobjects;
 
+import abstractComponents.AbstractComponent;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.FindAll;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class SequenceMemoryPage {
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class SequenceMemoryPage extends AbstractComponent {
     WebDriver driver;
 
     public SequenceMemoryPage(WebDriver driver) {
+        super(driver);
         // super(driver);
         // initialization
         this.driver = driver;
         PageFactory.initElements(driver, this);
     }
-    @FindBy(css = "div[class = \"view-go e18o0sx0 css-saet2v e19owgy77\"] div div div h1 div")
-    WebElement reactionTimeClickBtn;
+
+    @FindBy(css = "button[class='css-de05nr e19owgy710']")
+    WebElement sequenceMemoryStartTestBtn;
+
+    @FindBy(css = "span span:nth-child(2)")
+    WebElement levelNum;
+
+    @FindBy(css = "div[class = 'square active']")
+    WebElement activeSquareBtn;
 
     //Same locator on all test pages
-    @FindBy(css = "button[class=\"css-qm6rs9 e19owgy710\"]")
+    @FindBy(css = "button[class='css-qm6rs9 e19owgy710']")
     WebElement saveScoreBtn;
+
+    @FindAll({@FindBy(css = "div[class = 'square-row']")})
+    public List<WebElement> squareRows;
+
+    @FindBy(css = "div[class='square']")
+    WebElement individualSquareBtn;
+
+    @FindAll({@FindBy(css = "div[class='square']")})
+    public List<WebElement> squareBtns;
+
+    @FindAll({@FindBy(css = "div[class = 'square-row'] div")})
+    public List<WebElement> squareBtns1;
+
+
+    public List<Integer> getClickIndex() throws InterruptedException {
+        List<Integer> btnClickIndexs = new ArrayList<Integer>();
+        for(int i = 0; i<Integer.valueOf(levelNum.getText()); i++) {
+            for (WebElement x : squareBtns1) {
+                waitForWebElementToAppear(activeSquareBtn);
+                if (x.getAttribute("class").contains("active")) {
+                    btnClickIndexs.add(squareBtns1.indexOf(x));
+
+                    System.out.println("adding into btnclick "+squareBtns1.indexOf(x));
+                    System.out.println("indexlist size "+btnClickIndexs.size());
+                }
+            }
+            Thread.sleep(400);
+        }
+
+            return btnClickIndexs;
+
+    }
+
+    public void clickBoxes() throws InterruptedException {
+        for(int i = 0; i<Integer.valueOf(levelNum.getText()); i++) {
+            Iterator<Integer> it = getClickIndex().iterator();
+            while (it.hasNext()) {
+                Integer x = it.next();
+                Thread.sleep(400);
+
+                squareBtns1.get(x).click();
+            }
+
+        }
+    }
 
     public void goTo() {
         driver.get("https://humanbenchmark.com/tests/sequence");
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        SequenceMemoryPage sequenceMemoryPage = new SequenceMemoryPage(driver);
+        sequenceMemoryPage.goTo();
+        sequenceMemoryPage.sequenceMemoryStartTestBtn.click();
+        sequenceMemoryPage.clickBoxes();
+
     }
 }
